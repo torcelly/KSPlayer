ddf5fe94070e4e1b0fbf68c87b4fd206c61e43b6
Fernando <fernando@torcelly.com>
2025-08-20 01:13:23 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -61,6 +61,6 @@ if FileManager.default.fileExists(atPath: ffmpegKitPath + "/Package.swift") {
     ]
 } else {
     package.dependencies += [
-        .package(url: "https://github.com/nicolastinkl/FFmpegKit", from: "6.1.3"),
+        .package(url: "https://github.com/nicolastinkl/FFmpegKit.git", branch: "main")
     ]
 }
